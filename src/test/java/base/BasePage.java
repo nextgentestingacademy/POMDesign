aa6829c4b21023d44f3e6ed008890346661e9817
nextgentestingacademy <nextgentestingacademy@gmail.com>
2025-12-03 09:42:40 +0530

    BasePage

--- src/test/java/base/BasePage.java
+++ src/test/java/base/BasePage.java
@@ -1,9 +1,11 @@
 package base;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -15,6 +17,8 @@ public class BasePage {
 		this.driver = driver;
 		this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 	}
+	
+	
 	
 	
 	protected boolean waitForElementClickable(By locator) {
@@ -66,7 +70,46 @@ public class BasePage {
 			System.out.println("Unable to enter on " + locator + " :" + e.getMessage());
 			return false;
 		}
-		
+	}
+
+	protected int getTableRowCount(By locator) {
+		try {
+			WebElement table = driver.findElement(locator);
+			List rows = table.findElements(By.tagName("tr"));
+			return rows.size();
+		}catch(Exception e) {
+			System.out.println("Unable to get row count of " + locator + " :" + e.getMessage());
+			return -1;
+		}
 	}
 	
+	protected int getTableColCount(By locator) {
+		try {
+			WebElement table = driver.findElement(locator);
+			WebElement firstRow = table.findElement(By.tagName("tr"));
+			
+			List cols = firstRow.findElements(By.tagName("th"));
+			return cols.size();
+		}catch(Exception e) {
+			System.out.println("Unable to get column count of " + locator + " :" + e.getMessage());
+			return -1;
+		}
+	}
+	
+	protected String getCellText(By locator, int rowNum, int colNum) {
+		try {
+			WebElement table = driver.findElement(locator);
+			
+			List <WebElement> rows = table.findElements(By.tagName("tr"));
+			
+			WebElement row = rows.get(rowNum); 
+			
+			List <WebElement> cols = row.findElements(By.tagName("th"));
+			
+			return cols.get(colNum).getText().trim();
+		}catch(Exception e) {
+			System.out.println("Unable to get text of " + locator + " :" + e.getMessage());
+			return "";
+		}
+	}
 }
